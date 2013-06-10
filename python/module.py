0b453b87435fbde527417d61e5a8044bd8379f4b
Anton Hvornum <anton.feeds@gmail.com>
2013-06-10 17:29:47 +0300

    Update module.py

--- python/module.py
+++ python/module.py
@@ -9,14 +9,30 @@ def download(url):
 	
 def Import(name):
 	absname = name+'.py'
+	module = True
+	module_file = None
 	if not isfile(absname):
 		for line in download('https://raw.github.com/Torxed/Scripts/master/INDEX').split('\n'):
 			if len(line) <= 0 or line[0] == '#': continue
+		
 			index_modname, index_modlink = line.split(' - ',1)
-			if absname in index_modname:
-				with open(absname, 'wb') as fh_module:
+			if ' - ' in index_modlink:
+				index_functions = index_modlink.split(' - ',1).split(',')
+			else:
+				index_functions = []
+				
+			if absname in index_modname or name in index_functions:
+				module_file = index_modname
+				with open(index_modname, 'wb') as fh_module:
 					fh_module.write(download(index_modlink.strip()))
+				if name in index_functions:
+					module = False
 				break
-	if not isfile(absname):
+			
+	if module_file == None or not isfile(index_modname + '.py'):
 		return None
-	return __import__(name)
+		
+	if module:
+		return __import__(index_modname)
+	else:
+		return __import__(index_modname).name
