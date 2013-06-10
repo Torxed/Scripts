db04f3cc3ee751032591bf8050ed940dc6c38fea
Anton Hvornum <anton.feeds@gmail.com>
2013-06-10 15:26:31 +0200

    Update module.py

--- python/module.py
+++ python/module.py
@@ -1,7 +1,8 @@
-from urllib2 import urlopen
+from urllib2 import Request, urlopen
 from os.path import isfile
 def download(url):
-	x = urlopen(url)
+	req = Request(url)
+	x = urlopen(req)
 	data = x.read()
 	x.close()
 	return data
@@ -9,17 +10,13 @@ def download(url):
 def Import(name):
 	absname = name+'.py'
 	if not isfile(absname):
-		if not isfile('modules.index'):
-			with open('modules.index', 'wb') as fh:
-				fh.write(download('https://raw.github.com/Torxed/Scripts/master/INDEX'))
-		with open('modules.index', 'rb') as fh:
-			for line in fh:
-				if len(line) <= 0 or line[0] == '#': continue
-				index_modname, index_modlink = line.split(' - ',1)
-				if absname in index_modname:
-					with open(absname, 'wb') as fh_module:
-						fh_module.write(download(index_modlink.strip()))
-					break
+		for line in download('https://raw.github.com/Torxed/Scripts/master/INDEX').split('\n'):
+			if len(line) <= 0 or line[0] == '#': continue
+			index_modname, index_modlink = line.split(' - ',1)
+			if absname in index_modname:
+				with open(absname, 'wb') as fh_module:
+					fh_module.write(download(index_modlink.strip()))
+				break
 	if not isfile(absname):
 		return None
 	return __import__(name)
