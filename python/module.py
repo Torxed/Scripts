aa2692aece97d06cb641eb88f6cc7ff5c4be5fd1
Lord Anton Hvornum <anton.doxid@gmail.com>
2013-06-11 15:56:40 +0200

    Used sys.stdout instead of local_output in one place

--- python/module.py
+++ python/module.py
@@ -42,7 +42,7 @@ def Import(name):
 						key, val = func, ''
 					tmp[key] = val
 				index_functions = tmp
-				sys.stdout.write(str(index_functions)+'\n')
+				local_output(str(index_functions)+'\n')
 			else:
 				index_functions = {}
 				
