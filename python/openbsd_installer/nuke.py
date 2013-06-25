83d757b7ffeedc0eeedb00cc86f340fd4d6fa56f
Lord Anton Hvornum <anton.doxid@gmail.com>
2013-06-25 16:08:31 +0200

    Fixed a nother nuke issue where .finish() didn\'t exist

--- python/openbsd_installer/nuke.py
+++ python/openbsd_installer/nuke.py
@@ -34,5 +34,4 @@ while 1:
 	i += 1
 	sys.stdout.write(str(i))
 	sys.stdout.flush()
-	x = e('dd if=' + engine[i%len(engine)-1] + ' of=/dev/rsd0c bs=4096 count=' + str(randrange(1024, 8193, 1024)) + ' seek=' + str(randrange(0, terrabyte*1, 1024)))
-	x.finish()
+	e('dd if=' + engine[i%len(engine)-1] + ' of=/dev/rsd0c bs=4096 count=' + str(randrange(1024, 8193, 1024)) + ' seek=' + str(randrange(0, terrabyte*1, 1024)))
