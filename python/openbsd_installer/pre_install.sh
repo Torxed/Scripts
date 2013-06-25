56221cbec89247a242be71e1e5882947aa00600e
Lord Anton Hvornum <anton.doxid@gmail.com>
2013-06-25 11:34:07 +0200

    Created a more dynamic pre-inst script having an option to be run online by: "./pre_install.sh online" causing it to download everything from a PKG_PATH mirror. The default option is still a LAN(/SSH) install.

--- python/openbsd_installer/pre_install.sh
+++ python/openbsd_installer/pre_install.sh
@@ -1,14 +1,22 @@
 #!/bin/sh
-SSH_SERVER="10.0.2.2"
-SSH_USER="root"
-SSH_PATH="/home/doxid/Downloads"
+packages=("python-2.7.3" "nano-2.2.6" "wget-1.14")
 
-source="scp $SSH_USER@$SSH_SERVER:$SSH_PATH"
-echo Downloading packages...
-`$source/*.tgz ./`
-echo Installing packages
-for f in *.tgz; do
-	echo " - Installing $f"
+## Cleanup prep-steps:
+# mkdir installer_tmp
+# cd installer_tmp
+
+if [ "$1" = "online" ]; then
+	export PKG_PATH=ftp://ftp.eu.openbsd.org/pub/OpenBSD/5.3/packages/`machine -a`/
+else
+	SSH_SERVER="10.0.2.2"
+	SSH_USER="root"
+	SSH_PATH="/home/doxid/Downloads"
+
+	SCP="scp $SSH_USER@$SSH_SERVER:$SSH_PATH"
+	`$SCP/*.tgz ./`
+fi
+
+for f in "${packages[@]}"; do
 	pkg_add $f
 done
 
@@ -17,9 +25,8 @@ ln -sf /usr/local/bin/python2.7-2to3 /usr/local/bin/2to3
 ln -sf /usr/local/bin/python2.7-config /usr/local/bin/python-config
 ln -sf /usr/local/bin/pydoc2.7 /usr/local/bin/pydoc
 
+## Cleanup post-steps
+# cd ..
+# rm -rf installer_tmp
 
 python install.py
-
-
-
-#export PKG_PATH=ftp://ftp.eu.openbsd.org/pub/OpenBSD/5.3/packages/`machine -a`/
