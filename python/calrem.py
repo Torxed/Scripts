24b7603b9e8921e8c85dcc0bbf64125ff1872f55
Lord Anton Hvornum <anton.doxid@gmail.com>
2013-07-10 23:54:58 +0200

    Moved CalRem from it\'s own project into this bulk storage since it\'s no longer maintained or developed or used..

--- python/calrem.py
+++ python/calrem.py
@@ -2,19 +2,15 @@
 # -*- coding: latin-1 -*-
 
 __author__ = 'anton.doxid@gmail.com'
-__credit__ = 'api.rboyd@gmail.com (Ryan Boyd)', 'http://code.google.com/p/gdata-python-client/source/browse/samples/calendar/calendarExample.py'
+__credit__ = 'api.rboyd (Ryan Boyd)', 'http://code.google.com/p/gdata-python-client/source/browse/samples/calendar/calendarExample.py'
 
-import gdata.calendar.data
 import gdata.calendar.client
-import gdata.acl.data
 import sys, time, re, pynotify
 
 sys.setdefaultencoding('latin-1')
 
 def swe(s):
-	if s:
-		s = s.decode('utf-8', 'ignore')
-	return s
+	return s.decode('utf-8', 'ignore')
 
 def cleanDate(s):
 	return re.findall("[0-9]{4}-[0-9]{2}-[0-9]{2}", s)[0]
@@ -29,16 +25,19 @@ def getmail(s):
 	return re.findall('[\w\-][\w\-\.]+@[\w\-][\w\-\.]+[a-zA-Z]{1,4}', s)[0]
 
 class Calendar:
-	def __init__(self, email, password):
+	def __init__(self, email, password, remindwithin=3):
 		self.cal_client = gdata.calendar.client.CalendarClient(source='Google-Calendar_Python_Sample-1.0')
-		self.cal_client.ClientLogin(email, password, self.cal_client.source);
+		try:
+			self.cal_client.ClientLogin(email, password, self.cal_client.source);
+		except:
+			return False
 
 		self.allcalendars = self.cal_client.GetAllCalendarsFeed()
 		self.user = self.allcalendars.title.text
+		self.remindwithin = remindwithin
 
 	def getReminders(self):
 		start_date = time.strftime('%Y-%m-%d')#'2001-01-01'
-		end_date = '2013-01-30'
 		weekday = time.strftime("%a")
 
 		nextsunday = 86401
@@ -56,15 +55,20 @@ class Calendar:
 		for i, an_event in zip(xrange(len(feed.entry)), feed.entry):
 			for a_when in an_event.when:
 				t, s, e = swe(an_event.title.text), a_when.start, a_when.end
-				if time.time() - strftotime(s) <= 86400*3: # 3 days
+				if time.time() - strftotime(s) <= 86400*self.remindwithin: # default *3 days
 					pynotify.init("CalRem")
 					Hello=pynotify.Notification('[' + cleanDate(s) + ']', t)
 					Hello.show()
 
 if __name__ == '__main__':
 	if len(sys.argv) < 3:
-		print 'Requires to be run with: ./calrem.py <g-user> <g-pass>'
+		sys.stdout.write('Requires to be run with: ./calrem.py <g-user> <g-pass>\n')
 	else:
 		user, pw = sys.argv[1:3]
-		cal = Calendar(user, pw)
-		cal.getReminders()
+		cal = Calendar(user, pw, 3) # 3 days timeframe for notifications
+		if cal:
+			cal.getReminders()
+		else:
+			sys.stdout.write('Incorrect Google Username/Password!\n')
+
+	sys.stdout.flush()
