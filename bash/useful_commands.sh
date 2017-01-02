eb05af2deb806a68235717b6d8cf3729dd62f9b7
Anton Hvornum <anton.feeds@gmail.com>
2017-01-02 12:47:12 +0100

    Update useful_commands.sh

--- bash/useful_commands.sh
+++ bash/useful_commands.sh
@@ -1,1 +1,4 @@
+// Find a specific file/wildcard and output the size.
 find / -type f -name "$1" -exec ls -lh {} \; | awk '{ print $9 ": " $5 }'
+// List the 10 largest files from within the current directory.
+for i in G M K; do du -ah | grep [0-9]$i | sort -nr -k 1; done | head -n 11
