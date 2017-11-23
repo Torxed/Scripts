166af88799f717ec752359280ee6b4128cb2da6b
Anton Hvornum <anton.feeds@gmail.com>
2017-11-23 13:44:22 +0100

    Update qemu.sh

--- bash/qemu.sh
+++ bash/qemu.sh
@@ -1,2 +1,3 @@
  # Windows 10:
- qemu-system-x86_64 --enable-kvm -net nic -net user,smb=/tmp/shared/ -vga std -m 8196 -cdrom Downloads/Win10_1709_English_x64.iso -cpu host,hv_relaxed,hv_spinlocks=0x1fff,hv_vapic,hv_time -smp 2
+ qemu-img create -f qcow2 win10.img 25G
+ qemu-system-x86_64 --enable-kvm -hda win10.img -net nic -net user,smb=/tmp/shared/ -vga std -m 8196 -cdrom Downloads/Win10_1709_English_x64.iso -cpu host,hv_relaxed,hv_spinlocks=0x1fff,hv_vapic,hv_time -smp 2
