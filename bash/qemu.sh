5e462339be5d5b1984f905a0db5206b194d055aa
Anton Hvornum <anton.feeds@gmail.com>
2018-05-28 20:20:14 +0200

    Update qemu.sh

--- bash/qemu.sh
+++ bash/qemu.sh
@@ -1,3 +1,32 @@
- # Windows 10:
- qemu-img create -f qcow2 win10.img 25G
- qemu-system-x86_64 --enable-kvm -hda win10.img -net nic -net user,smb=/tmp/shared/ -vga std -m 8196 -cdrom Downloads/Win10_1709_English_x64.iso -cpu host,hv_relaxed,hv_spinlocks=0x1fff,hv_vapic,hv_time -smp 2
+#!/bin/bash
+
+MAC='00:11:22:33:44:55'
+TAP='tap0'
+CD=''
+IMAGE='test_deploy.qcow2'
+
+if [[ -n $1 ]]; then
+  $CD="-cdrom $1"
+fi
+
+# Windows 10:
+# -cpu host,hv_relaxed,hv_spinlocks=0x1fff,hv_vapic,hv_time -smp 2
+
+if [[ -z $(ip addr | grep ${TAP}) ]]; then
+  sudo ip tuntap add dev ${TAP} mode tap user torxed group torxed
+  sudo ip link set dev ${TAP} master br0
+  sudo ip link set dev ${TAP} up
+fi
+
+# -device e1000,netdev=network0,mac=${MAC} -netdev tap,id=network0,ifname=${TAP},script=no,downscript=no
+qemu-system-x86_64 -enable-kvm -machine q35,accel=kvm -device intel-iommu \
+  -cpu host \
+  -mem 4096 \
+  -netdev tap,ifname=${TAP},id=network0,script=no,downscript=no -device i82559c,netdev=network0,mac=${MAC} \
+  ${CD} \
+  -drive file=${IMAGE},format=qcow2 \
+  -boot order=d \
+  -drive if=pflash,format=raw,readonly,file=/usr/share/ovmf/x64/OVMF_CODE.fd \
+  -drive if=pflash,format=raw,readonly,file=/usr/share/ovmf/x64/OVMF_VARS.fd
+
+ip tuntap delete dev tap0 mode tap0
