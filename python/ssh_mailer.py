7b5af6b21b72ddf10f024e64b1dd55d2aa9a1325
Anton Hvornum <anton.feeds+github@gmail.com>
2019-10-23 11:51:16 +0200

    Refactored into functions and templates

--- python/ssh_mailer.py
+++ python/ssh_mailer.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python
 
-import smtplib, dns.resolver, os, logging, psutil, socket, ssl, dkim
+## Requirements:
+#  * python-dnspython
+#  * python-dkim
+#  * python-pygeoip
+
+import smtplib, dns.resolver, os, logging, psutil, socket, ssl, dkim, json
 from time import time, localtime
 from hashlib import md5
 from systemd.journal import JournalHandler
@@ -9,13 +14,14 @@ from email import encoders
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
-from os.path import isfile
+from os.path import isfile, dirname, isdir
 from json import load as jload, dump as jdump
 
 log = logging.getLogger('ssh-gateway')
 log.addHandler(JournalHandler())
 log.setLevel(logging.WARNING)
 
+## Load the cached RESOLVE hits from disk:
 cache = {}
 if isfile('/tmp/sshmailer.json'):
 	with open('/tmp/sshmailer.json', 'r') as fh:
@@ -24,105 +30,64 @@ if isfile('/tmp/sshmailer.json'):
 		except:
 			pass # Invalid format, probably a crash or something.
 
-t = time()
-DOMAIN = 'example.com'
-FROM = f'ssh@{DOMAIN}'
-TO = f'reciever@example.com'
-meta = {
-	'USER' : None,
-	'IP' : None,
-	'RESOLV' : None,
-	'ENVIRON' : os.environ,
-	'PARAMS' : argv,
-	'USERS' : psutil.users(),
-	'DOMAIN' : DOMAIN,
-	'SSH_MAIL_USER_FROM' : 'ssh',
-	'SSH_MAIL_USER_TO' : 'reciever',
-	'SSH_MAIL_USER_TODOMAIN' : 'example.com',
-	'TIME' : '{}-{}-{} {}:{}:{}'.format(*localtime(t)),
-	'RAW_TIME' : t,
-	'SUBJECT' : None,
-	'COUNTRY' : None,
-	'Message-ID' : '<{}.{}@{}>'.format(time(), md5(bytes(f'{FROM}.{TO}', 'UTF-8')).hexdigest(), DOMAIN)
-}
+def get_ip():
+	if 'SSH_CONNECTION' in os.environ:
+		return os.environ['SSH_CONNECTION'].split()[0]
+	elif 'PAM_RHOST' in os.environ:
+		return os.environ['PAM_RHOST']
+	
+	log.warning("Missing remote IP in environment variables: {}".format(os.environ))
+	return '127.0.0.1' # Hard to detect, maybe dump all of netstat -an | grep :22?
 
-if 'PAM_TYPE' in os.environ and os.environ['PAM_TYPE'] == 'close_session':
-	meta['SUBJECT'] = 'SSH Logout: {USER}@{DOMAIN}'.format(**meta)
-else:
-	meta['SUBJECT'] = 'SSH Login: {USER}@{DOMAIN}'.format(**meta)
+def country(ip):
+	if isfile(configuration['GEOIP_DB']):
+		import pygeoip
+		gi = pygeoip.GeoIP(configuration['GEOIP_DB'])
+		result = gi.country_name_by_addr(ip)
+		if result == '' or not result:
+			result = 'Unknown Country'
+		return result
+	
+	log.warning("Missing GeoIP database at: {GEOIP_DB}".format(**configuration))
+	return "Unknown"
 
-if 'SSH_CONNECTION' in os.environ:
-	meta['IP'] = os.environ['SSH_CONNECTION'].split()[0]
-elif 'PAM_RHOST' in os.environ:
-	meta['IP'] = os.environ['PAM_RHOST']
-else:
-	meta['IP'] = '127.0.0.1' # Hard to detect, maybe dump all of netstat -an | grep :22?
-	log.warning("{}".format(os.environ))
+def resolv(ip):
+	if ip in cache and time()-cache[ip]['time']<86400:
+		return cache[ip]['resolved']
 
-if isfile('/usr/share/GeoIP/GeoIP.dat'):
-	import pygeoip
-	gi = pygeoip.GeoIP('/usr/share/GeoIP/GeoIP.dat')
-	meta['COUNTRY'] = gi.country_name_by_addr(meta['IP'])
-else:
-	meta['COUNTRY'] = "Unknown"
-
-if meta['IP'] in cache and time()-cache[meta['IP']]['time']<86400:
-	meta['RESOLV'] = cache[meta['IP']]['resolved']
-else:
 	try:
-		meta['RESOLV'] = socket.gethostbyaddr(meta['IP'])
-		if meta['RESOLV']:
-			meta['RESOLV'] = ':'.join(str(x) for x in meta['RESOLV'])
-		cache[meta['IP']] = {'time' : time(), 'resolved' : meta['RESOLV']}
-	except:
-		meta['RESOLV'] = meta['IP']
-
-## Save the cached resolves for speed up reasons
-with open('/tmp/sshmailer.json', 'w') as fh:
-	try:
-		jdump(cache, fh)
+		result = socket.gethostbyaddr(ip)
+		if result:
+			result = result[0]
+			#result = ':'.join(str(x) for x in result)
+		cache[ip] = {'time' : time(), 'resolved' : result}
+		return cache[ip]['resolved']
 	except:
 		pass
+	
+	return ip
 
-if 'USER' in os.environ:
-	meta['USER'] = os.environ['USER']
-elif 'PAM_USER' in os.environ:
-	meta['USER'] = os.environ['PAM_USER']
-else:
-	meta['USER'] = 'root' # Just default and scare the shit out of us!
-	log.warning("{}".format(os.environ))
-
-log.warning("User {USER} Logged in from {IP}.".format(**meta))
-
-## TODO: https://support.google.com/mail/answer/81126
-## TODO:(DKIM) https://russell.ballestrini.net/quickstart-to-dkim-sign-email-with-python/
-## TODO:(S/MIME) https://tools.ietf.org/doc/python-m2crypto/howto.smime.html
-## TODO: https://support.rackspace.com/how-to/create-an-spf-txt-record/
-##
-## https://toolbox.googleapps.com/apps/checkmx/check?domain={DOMAIN}&dkim_selector=
-## https://github.com/PowerDNS/pdns/issues/2881
-
-email = MIMEMultipart('alternative')
-email['Subject'] = "SSH Login: {USER}@{DOMAIN}".format(**meta)
-email['From'] = "SSH Guard <{SSH_MAIL_USER_FROM}@{DOMAIN}>".format(**meta)
-email['To'] = "Anton Hvornum <{SSH_MAIL_USER_TO}@{SSH_MAIL_USER_TODOMAIN}>".format(**meta)
-email['Message-ID'] = meta['Message-ID']
-email.preamble = 'SSH Login {USER}@{DOMAIN}'.format(**meta)
-
-t = time()
-text = """\
+def load_text_template():
+	template = """\
 The following information is available:
  * Remote-IP: {IP} [{COUNTRY}] ({RESOLV})
  * Time of occurance: {TIME} ({RAW_TIME})
  * Environment variable: {ENVIRON}
  * Parameters: {PARAMS}
  * Logged On Users: {USERS}
-""".format(**meta)
+"""
+	if not isdir(dirname(configuration['TEXT_TEMPLATE'])):
+		return template.format(**configuration)
 
-if meta['COUNTRY'].lower() != 'sweden':
-	meta['COUNTRY'] = '<font color="red">{COUNTRY}</font>'.format(**meta)
+	if not isfile(configuration['TEXT_TEMPLATE']):
+		with open(configuration['TEXT_TEMPLATE'], 'w') as fh:
+			fh.write(template)
 
-html = """\
+	with open(configuration['TEXT_TEMPLATE'], 'r') as fh:
+		return fh.read().format(**configuration)
+
+def load_html_template():
+	template = """\
 <html>
 	<head>
 		<title>{SUBJECT}</title>
@@ -139,7 +104,113 @@ html = """\
 			</ul>
 		</div>
 	</body>
-</html>""".format(**meta)
+</html>"""
+
+	if not isdir(dirname(configuration['HTML_TEMPLATE'])):
+		return template.format(**configuration)
+
+	if not isfile(configuration['HTML_TEMPLATE']):
+		with open(configuration['HTML_TEMPLATE'], 'w') as fh:
+			fh.write(template)
+
+	with open(configuration['HTML_TEMPLATE'], 'r') as fh:
+		return fh.read().format(**configuration)
+
+def sign_email(email, selector='default', domain=None):
+	if not domain: domain = configuration['DOMAIN']
+	if type(selector) != bytes: selector = bytes(selector, 'UTF-8')
+	if type(domain) != bytes: domain = bytes(domain, 'UTF-8')
+
+	if not isdir(dirname(configuration['DKIM_KEY'])):
+		log.warning('Missing DKIM key: {DKIM_KEY}'.format(**configuration))
+		return None 
+	if not isfile(configuration['DKIM_KEY']):
+		log.warning('Missing DKIM key: {DKIM_KEY}'.format(**configuration))
+		return None
+
+	with open(configuration['DKIM_KEY'], 'rb') as fh:
+		dkim_private_key = fh.read()
+
+	sig = dkim.sign(message=bytes(email.as_string(), 'UTF-8'),
+					selector=selector,
+					domain=domain,
+					privkey=dkim_private_key,
+					include_headers=["To", "From", "Subject"])
+
+	return sig.lstrip(b"DKIM-Signature: ").decode('UTF-8')
+
+def get_logedonuser():
+	if 'USER' in os.environ:
+		return os.environ['USER']
+	elif 'PAM_USER' in os.environ:
+		return os.environ['PAM_USER']
+	else:
+		log.warning("Missing USER definition in environment: {}".format(os.environ))
+	
+	return 'root' # Just a default to scare the shit out of us!
+
+t = time()
+configuration = {
+	'DOMAIN' : 'example.com',
+	'SSH_MAIL_USER_FROM' : 'ssh', # ssh@example.com
+	'SSH_MAIL_USER_TO' : 'reciever', # reciever@example.com
+	'SSH_MAIL_USER_TODOMAIN' : 'example.com',
+	'SSH_MAIL_TO_REALNAME' : 'Recievers Name'
+	'RAW_TIME' : t,
+	'TIME' : '{}-{}-{} {}:{}:{}'.format(*localtime(t)),
+	'SUBJECT' : None,
+	'COUNTRY' : None,
+	'USER' : None,
+	'IP' : None,
+	'RESOLV' : None,
+	'ENVIRON' : dict(os.environ),
+	'PARAMS' : argv,
+	'USERS' : psutil.users(),
+	'TRY_ONE_MAILSERVER' : False,
+	'TEXT_TEMPLATE' : '/etc/sshmailer/text.template',
+	'HTML_TEMPLATE' : '/etc/sshmailer/html.template',
+	'DKIM_KEY' : "/etc/sshmailer/sshmailer.pem",
+	'GEOIP_DB' : '/usr/share/GeoIP/GeoIP.dat'
+}
+
+configuration['USER'] = get_logedonuser()
+configuration['HASH'] = md5(bytes('{SSH_MAIL_USER_FROM}@{DOMAIN}+{SSH_MAIL_USER_TO}@{SSH_MAIL_USER_TODOMAIN}'.format(**configuration), 'UTF-8')).hexdigest()
+configuration['Message-ID'] = '<{RAW_TIME}.{HASH}@{DOMAIN}>'.format(**configuration)
+configuration['SUBJECT'] = 'SSH Session: {USER}@{DOMAIN}'.format(**configuration)
+
+configuration['IP'] = get_ip()
+configuration['RESOLV'] = resolv(configuration['IP'])
+configuration['COUNTRY'] = country(configuration['IP'])
+if configuration['COUNTRY'].lower() != 'sweden':
+	configuration['COUNTRY'] = '<font color="red">{COUNTRY}</font>'.format(**configuration)
+
+log.warning("User {USER} Logged in from {IP}.".format(**configuration))
+
+## Save the cached resolves for speed up reasons
+with open('/tmp/sshmailer.json', 'w') as fh:
+	try:
+		jdump(cache, fh)
+	except:
+		pass
+
+## TODO: https://support.google.com/mail/answer/81126
+## TODO:(DKIM) https://russell.ballestrini.net/quickstart-to-dkim-sign-email-with-python/
+## TODO:(S/MIME) https://tools.ietf.org/doc/python-m2crypto/howto.smime.html
+## TODO: https://support.rackspace.com/how-to/create-an-spf-txt-record/
+##
+## https://toolbox.googleapps.com/apps/checkmx/check?domain={DOMAIN}&dkim_selector=
+## https://github.com/PowerDNS/pdns/issues/2881
+
+email = MIMEMultipart('alternative')
+email['Subject'] = configuration['SUBJECT']
+email['From'] = "SSH Guard <{SSH_MAIL_USER_FROM}@{DOMAIN}>".format(**configuration)
+email['To'] = "{SSH_MAIL_TO_REALNAME} <{SSH_MAIL_USER_TO}@{SSH_MAIL_USER_TODOMAIN}>".format(**configuration)
+email['Message-ID'] = configuration['Message-ID']
+email.preamble = configuration['SUBJECT']
+
+text = load_text_template()
+html = load_html_template()
+
 
 email_body_text = MIMEText(text, 'plain')
 email_body_html = MIMEBase('text', 'html')
@@ -150,12 +221,7 @@ email_body_html.set_charset('UTF-8')
 email.attach(email_body_text)
 email.attach(email_body_html)
 
-with open("/etc/sshmailer/sshmailer.pem", 'rb') as fh:
-	dkim_private_key = fh.read()
-
-headers = ["To", "From", "Subject"]
-sig = dkim.sign(message=bytes(email.as_string(), 'UTF-8'), selector=b"default", domain=bytes(meta['DOMAIN'], 'UTF-8'), privkey=dkim_private_key, include_headers=headers)
-email["DKIM-Signature"] = sig.lstrip(b"DKIM-Signature: ").decode('UTF-8')
+email["DKIM-Signature"] = sign_email(email)
 
 context = ssl.create_default_context()
 for mx_record in dns.resolver.query('gmail.com', 'MX'):
@@ -163,13 +229,16 @@ for mx_record in dns.resolver.query('gmail.com', 'MX'):
 	try:
 		server = smtplib.SMTP(mail_server, port=25, timeout=10) # 587 = TLS, 465 = SSL
 		if server.starttls(context=context)[0] != 220:
-			raise ValueError('Could not start TLS.')
-		#server = smtplib.SMTP_SSL(mail_server)
-		server.sendmail(FROM, TO, email.as_string())
+			log.warning('Could not start TLS.')
+		
+		server.sendmail('{SSH_MAIL_USER_FROM}@{DOMAIN}'.format(**configuration), '{SSH_MAIL_USER_TO}@{SSH_MAIL_USER_TODOMAIN}'.format(**configuration), email.as_string())
 		server.quit()
 	#		server.close()
 		break
 	except Exception as e:
-		log.warning("Could not notify our chief of command @ {}!!".format(mail_server))
+		log.warning("Could not send email via: {}!!".format(mail_server))
 		log.warning("{}".format(e))
+
+	if configuration['TRY_ONE_MAILSERVER']:
+		break
 
