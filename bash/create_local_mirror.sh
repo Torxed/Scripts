0ffefb2840e35c8c7ed3a06e8e5221176b0ef8e0
Anton Hvornum <anton.feeds+github@gmail.com>
2020-03-14 14:16:25 +0100

    Update create_local_mirror.sh

--- bash/create_local_mirror.sh
+++ bash/create_local_mirror.sh
@@ -3,10 +3,12 @@
 TMP_DB="/tmp/pacdb"
 ROOT_PATH="/srv/http/local_mirror"
 BIN_PATH="os/x86_64"
+# expac simply resolves dependencies, splits them into new lines and tr just wraps them on the same line
+PACKAGES=$(expac -S '%E' -l '\n' base base-devel linux linux-firmware btrfs-progs efibootmgr nano wpa_supplicant dialog nano lollypop gstreamer gst-plugins-good gnome-keyring nemo gpicview-gtk3 chromium awesome xorg-server xorg-xrandr xorg-xinit xterm feh slock xscreensaver terminus-font-otb gnu-free-fonts ttf-liberation xsel qemu ovmf openssh sshfs git htop pkgfile scrot dhclient wget smbclient cifs-utils libu2f-host pulseaudio pulseaudio-alsa pavucontrol python git python-psutil python-systemd python-pygeoip geoip-database syslinux | tr '\n' ' ')
 
 mkdir -p "${TMP_DB}"
 mkdir -p "${BIN_PATH}"
-sudo pacman --dbpath "${TMP_DB}" -Syu -w --root "${ROOT_PATH}" --cachedir "${BIN_PATH}" base base-devel linux linux-firmware btrfs-progs efibootmgr nano wpa_supplicant dialog nano lollypop gstreamer gst-plugins-good gnome-keyring nemo gpicview-gtk3 chromium awesome xorg-server xorg-xrandr xorg-xinit xterm feh slock xscreensaver terminus-font-otb gnu-free-fonts ttf-liberation xsel qemu ovmf openssh sshfs git htop pkgfile scrot dhclient wget smbclient cifs-utils libu2f-host pulseaudio pulseaudio-alsa pavucontrol git python python-psutil python-systemd python-pygeoip geoip-database syslinux
+sudo pacman --dbpath "${TMP_DB}" -Syu -w --root "${ROOT_PATH}" --cachedir "${BIN_PATH}" "${PACKAGES}"
 sudo repo-add "${ROOT_PATH}/${BIN_PATH}/local_repo.db.tar.gz" "${ROOT_PATH}/${BIN_PATH}/*.pkg.tar.xz"
 
 cat <<EOF
