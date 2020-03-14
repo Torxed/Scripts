9902e2952374481554919e95ab3b427b5dcbe911
Anton Hvornum <anton.feeds+github@gmail.com>
2020-03-14 14:46:58 +0100

    expac seams to be causing issues

--- bash/create_local_mirror.sh
+++ bash/create_local_mirror.sh
@@ -4,7 +4,8 @@ TMP_DB="/tmp/pacdb"
 ROOT_PATH="/srv/http/local_mirror"
 BIN_PATH="os/x86_64"
 # expac simply resolves dependencies, splits them into new lines and tr just wraps them on the same line
-PACKAGES=$(expac -S '%E' -l '\n' base base-devel linux linux-firmware btrfs-progs efibootmgr nano wpa_supplicant dialog nano lollypop gstreamer gst-plugins-good gnome-keyring nemo gpicview-gtk3 chromium awesome xorg-server xorg-xrandr xorg-xinit xterm feh slock xscreensaver terminus-font-otb gnu-free-fonts ttf-liberation xsel qemu ovmf openssh sshfs git htop pkgfile scrot dhclient wget smbclient cifs-utils libu2f-host pulseaudio pulseaudio-alsa pavucontrol python git python-psutil python-systemd python-pygeoip geoip-database syslinux haveged intel-ucode amd-ucode memtest86+ mkinitcpio-nfs-utils nbd zsh efitools | tr '\n' ' ')
+PACKAGES="base base-devel linux linux-firmware btrfs-progs efibootmgr nano wpa_supplicant dialog nano lollypop gstreamer gst-plugins-good gnome-keyring nemo gpicview-gtk3 chromium awesome xorg-server xorg-xrandr xorg-xinit xterm feh slock xscreensaver terminus-font-otb gnu-free-fonts ttf-liberation xsel qemu ovmf openssh sshfs git htop pkgfile scrot dhclient wget smbclient cifs-utils libu2f-host pulseaudio pulseaudio-alsa pavucontrol python git python-psutil python-systemd python-pygeoip geoip-database syslinux haveged intel-ucode amd-ucode memtest86+ mkinitcpio-nfs-utils nbd zsh efitools"
+#PACKAGES=$(expac -S '%E' -l '\n' ${PACKAGES} | tr '\n' ' ')
 
 mkdir -p "${TMP_DB}"
 mkdir -p "${BIN_PATH}"
