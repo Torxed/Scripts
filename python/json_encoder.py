e5a2fcaa44326817a17e4000a61b7917e93a581f
Anton Hvornum <anton.feeds+github@gmail.com>
2020-03-28 20:40:43 +0100

    Update json_encoder.py

--- python/json_encoder.py
+++ python/json_encoder.py
@@ -1,24 +1,9 @@
 from json import JSONEncoder, dumps, loads
+from datetime import date, datetime
 
-class JSON_Typer(JSONEncoder):
-	def _encode(self, obj):
-		## Workaround to handle keys in the dictionary being bytes() objects etc.
-		## Also handles recursive JSON encoding. In case sub-keys are bytes/date etc.
-		##
-		## README: If you're wondering why we're doing loads(dumps(x)) instad of just dumps(x)
-		##         that's because it would become a escaped string unless we loads() it back as
-		##         a regular object - before getting passed to the super(JSONEncoder) which will
-		##         do the actual JSON encoding as it's last step. All this shananigans are just
-		##         to recursively handle different data types within a nested dict/list/X struct.
+class JSON_Encoder:
+	def _encode(obj):
 		if isinstance(obj, dict):
-			def check_key(o):
-				if type(o) == bytes:
-					o = o.decode('UTF-8', errors='replace')
-				elif type(o) == set:
-					o = loads(dumps(o, cls=JSON_Typer))
-				elif type(o) in (custom_class, custom_class_two):
-					o = o.dump()
-				return o
 			## We'll need to iterate not just the value that default() usually gets passed
 			## But also iterate manually over each key: value pair in order to trap the keys.
 			
@@ -28,14 +13,14 @@ class JSON_Typer(JSONEncoder):
                                                             # But it's the only quick way I can think of to 
                                                             # trigger a encoding of sub-dictionaries. (I'm also very tired, yolo!)
 				else:
-					val = check_key(val)
+					val = JSON_Encoder._encode(val)
 				del(obj[key])
-				obj[check_key(key)] = val
+				obj[JSON_Encoder._encode(key)] = val
 			return obj
+		elif hasattr(obj, 'json'):
+			return obj.json()
 		elif isinstance(obj, (datetime, date)):
 			return obj.isoformat()
-		elif isinstance(obj, (custom_class, custom_class_two)):
-			return loads(dumps(obj.dump(), cls=JSON_Typer))
 		elif isinstance(obj, (list, set, tuple)):
 			r = []
 			for item in obj:
@@ -43,6 +28,10 @@ class JSON_Typer(JSONEncoder):
 			return r
 		else:
 			return obj
+
+class JSON_Typer(JSONEncoder):
+	def _encode(self, obj):
+		return JSON_Encoder._encode(obj)
 
 	def encode(self, obj):
 		return super(JSON_Typer, self).encode(self._encode(obj))
