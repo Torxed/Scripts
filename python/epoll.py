086aec2ade3bc69a91998e2b8fc893699218d348
Anton Hvornum <anton.feeds+github@gmail.com>
2020-10-07 16:59:31 +0200

    Windows version of "epoll()"
    Based on select. But mimics the function-calls of `epoll()` for a cross-platform way of expecting the same result.

--- python/epoll.py
+++ python/epoll.py
@@ -1,43 +1,28 @@
-from socket import socket as so, SOL_SOCKET, SO_REUSEADDR, SHUT_RDWR
-from select import epoll, EPOLLIN, EPOLLHUP
+try:
+	from select import epoll, EPOLLIN
+except:
+	import select
+	EPOLLIN = None
+	class epoll():
+		""" #!if windows
+		Create a epoll() implementation that simulates the epoll() behavior.
+		This so that the rest of the code doesn't need to worry weither we're using select() or epoll().
+		"""
+		def __init__(self):
+			self.sockets = {}
+			self.monitoring = {}
 
-sock = so()
-sock.bind(('', 1337))
-sock.listen(4)
-sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-mainFid = sock.fileno()
+		def unregister(self, fileno, *args, **kwargs):
+			try:
+				del(self.monitoring[fileno])
+			except:
+				pass
 
-polly = epoll()
-polly.register(mainFid, EPOLLIN)
+		def register(self, fileno, *args, **kwargs):
+			self.monitoring[fileno] = True
 
-socks = {}
-
-while 1:
-        for fid, eid in polly.poll(1):
-                print(eid)
-                if fid == mainFid:
-                        ns, na = sock.accept()
-                        polly.register(ns.fileno(), EPOLLIN|EPOLLHUP)
-                        print(na[0],'connected')
-                        socks[ns.fileno()] = {'sock' : ns, 'addr' : na}
-                elif fid in socks and eid == EPOLLIN:
-                        data = socks[fid]['sock'].recv(8192)
-                        if data == b'':
-                                # We don't allow empty data, mostly because it
-                                # is the only way we can detect a disconnect with epoll.
-                                socks[fid]['sock'].shutdown(SHUT_RDWR)
-                        else:
-                                print(socks[fid]['addr'][0], ':]', data)
-                elif fid in socks and eid == 17: #SHUT
-                        # We closed the connection
-                        print('We closed a connection')
-                        polly.unregister(fid)
-                        socks[fid]['sock'].close()
-                        del(socks[fid])
-
-for key, vals in socks.items():
-        polly.unregister(key)
-        vals['sock'].close()
-
-polly.unregister(mainFid)
-sock.close()
+		def poll(self, timeout=0.05, *args, **kwargs):
+			try:
+				return [[fileno, 1] for fileno in select.select(list(self.monitoring.keys()), [], [], timeout)[0]]
+			except OSError:
+				return []
