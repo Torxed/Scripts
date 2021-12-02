468fb3237e0146f9f33b6dad4d6114a6bed269b9
Anton Hvornum <anton@hvornum.se>
2021-12-03 00:30:37 +0100

    Added type annotations

--- python/dupedict.py
+++ python/dupedict.py
@@ -1,14 +1,16 @@
-class DupeDict(dict):
+from typing import Optional, Any, List, Tuple, Iterator, Dict
+
+class DupeDict(dict[Any, Any]):
 	"""
 	DupeDict is a simple attempt at trying to support
 	multiple identical keys in a dictionary, while remaining some speed.
 	There's probably an academic word for this but I just call it DuplicateDict.
 	"""
-	internal_list = None
+	internal_list :Optional[List[Tuple[Any, Any]]] = None
 
-	def __setitem__(self, key, value):
+	def __setitem__(self, key :Any, value :Any) -> None:
 		if self.internal_list is None:
-			self.internal_list = []
+			self.internal_list :List[Tuple[Any, Any]] = []
 
 		index = len(self.internal_list)
 		self.internal_list.append((key, value))
@@ -17,24 +19,20 @@ class DupeDict(dict):
 		else:
 			dict.__getitem__(self, key).append(index)
 
-	def __getitem__(self, key):
-		indexes = dict.__getitem__(self, key)
-		for index in indexes:
-			yield dict([self.internal_list[index]])
+	def __getitem__(self, key :Any) -> Iterator[Any]:
+		if self.internal_list:
+			indexes = dict.__getitem__(self, key)
+			for index in indexes:
+				yield dict([self.internal_list[index]])
 
-	def __iter__(self):
-		for key in dict.__iter__(self):
-			for index in dict.__getitem__(self, key):
-				yield self.internal_list[index]
+	def __iter__(self) -> Iterator[Any]:
+		if self.internal_list:
+			for key in dict.__iter__(self):
+				for index in dict.__getitem__(self, key):
+					yield self.internal_list[index]
 
-	def __repr__(self):
-		return str([dict([x]) for x in self.internal_list])
-
-if __name__ == '__main__':
-	base_hardware = DupeDict()
-	base_hardware["cpu"] = "host"
-	base_hardware["device"] = "if=pflash,format=raw,readonly=on,file=/usr/share/ovmf/x64/OVMF_CODE.fd"
-	base_hardware["device"] = "if=pflash,format=raw,readonly=on,file=/usr/share/ovmf/x64/OVMF_VARS.fd"
-
-	for key, val in struct: # Haven't fixed .items(), it's implied
-		print(key, val)
+	def __repr__(self) -> str:
+		if self.internal_list:
+			return str([dict([x]) for x in self.internal_list])
+		else:
+			return ''
