cb776f50855b96d4228456ca58d67850c64028ce
Anton Hvornum <anton@hvornum.se>
2023-04-28 01:48:19 +0200

    Update shadow_hash.py

--- python/shadow_hash.py
+++ python/shadow_hash.py
@@ -7,3 +7,8 @@ password = '1234'
 salt = base64.b64encode(os.urandom(12))
 password = base64.b64encode(hashlib.pbkdf2_hmac('sha512', bytes(password, 'UTF-8'), salt, 200000, 64))
 print(f"SHA512 :: $6${salt.decode('UTF-8')}${password.decode('UTF-8')}")
+
+# Verify with (not sure why crypt.crypt(password, password_hash) doesn't work):
+def validate_hash(password, password_hash):
+	* _, salt, password_hash = password_hash.split('$')
+	return base64.b64encode(hashlib.pbkdf2_hmac('sha512', password.encode(), salt.encode(), 200000, 64)).decode() == password_hash
